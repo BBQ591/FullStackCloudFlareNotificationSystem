8c990bdf2f4730159dba00e4bc45a623dcbc7a4a
Brian Qu <bbq@Brians-MacBook-Pro.local>
2024-10-15 14:16:10 -0500

    deleted p

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
       <div key={notification.id} className="notification-card" style={{backgroundColor: color}}>
         <p className="notification-message">{notification.content.text}</p>
         <div className="notification-timestamp">
-          <p>{new moment(notification.timestamp).format("DD MMM YYYY, h:mma")}</p>
+          {new moment(notification.timestamp).format("DD MMM YYYY, h:mma")}
         </div>
       </div>
     )})
