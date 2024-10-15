2423a20f2b139e89e7328cc85b92847a6cc5a012
Brian Qu <bbq@Brians-MacBook-Pro.local>
2024-10-15 12:44:09 -0500

    finally fixed ai

--- functions/api/ai.js
+++ functions/api/ai.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
     console.log(env.llama10);
     if (request.method == "POST") {
         const requestBody = await request.json();
-        const stream = await env.llama10.run("@cf/meta/llama-3-8b-instruct", {
+        const stream = await env.llama3.run("@cf/meta/llama-3-8b-instruct", {
             stream: true,
             // max_tokens: 512,
             messages: [
