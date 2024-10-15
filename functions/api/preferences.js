3175161713d90eacad1ea5249b9562b32df3c64e
Brian Qu <bbq@Brians-MacBook-Pro.local>
2024-10-15 10:29:09 -0500

    added preferences

---
+++ functions/api/preferences.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+    // const url = new String(request.url);
+    let {env} = context;
+    let kv1 = env.kv1;
+    let preferences = JSON.stringify({"displayDuration": 5000, "preferredTypes": ["alert", "info"]});
+    kv1.put("preferences", preferences);
+    const response = new Response(preferences, { status: 200});
+    response.headers.append('Set-Cookie', 'preferences=' + preferences);
+    return response;
+}
+
+// export default {
+//     async fetch() {
+//         return new Response("hello", {status:200});
+//         // return onRequest(request, env);
+//     },
+// };
